ef37d26b33b0109e6b85480c111f82e7e830672c
ansaryergesh <yergeshovansar@gmail.com>
2020-06-24 15:42:52 +0600

    favour api

--- app/controllers/api/v1/favourites_controller.rb
+++ app/controllers/api/v1/favourites_controller.rb
@@ -2,13 +2,36 @@ class Api::V1::FavouritesController < ApplicationController
     before_action :set_home
     before_action :fint_favour, only: [:destroy]
 
+    def create
+        if favoure?
+            destroy
+        else
+            @home.favourites.create(user_id: current_user.id)
+            render json: @favour, status: :created
+        end
+    end
+    def index
+        if params[:user_id]
+            if params[:home_id]
+                @favourHomes = Favourite.where('user_id=? AND event_id=?', params[:user_id], params[:home_id]).first
+            end
+        else
+            @favourHomes = Favourite.all
+        end
+
+        render json: @favourHomes
+    end
+
+    def destroy
+        @favour.destroy
+    end
 
     private
 
     def set_home
         @home = Home.find(params[:home_id])
     end
-    
+
     def find_favour
         @favour= @home.favourites.find(params[:id])
     end
