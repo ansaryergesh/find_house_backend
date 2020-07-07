cb8a83825ee0977eb34389c991e5bb54e4461fe0
ansaryergesh <yergeshovansar@gmail.com>
2020-07-07 21:14:55 +0600

    test done

---
+++ spec/models/favourite_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Favourite, type: :model do
+    describe "Associations" do
+        it { should belong_to(:user)}
+        it { should belong_to(:home) }
+    end
+end
